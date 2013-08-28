4533380e7d12822c224eec7eb27dbacaa227dfa1
geun <geunbaelee@gmail.com>
2013-08-29 04:05:56 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubberTestApp::Application.config.secret_token = '25b50151b3d82425191ddf2013d5813764fa7b68e83c34ba9e492dac335a90f198036d9fc16cc44bfefa68979d20ce86e3ca0eefa6c4829f3b8dc99ef102977b'
